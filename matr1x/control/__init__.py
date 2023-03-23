43f3fefd7d4801936e29d9be6c16f00ac1ce7bb5
Dominik Kriegner <dominik.kriegner@gmail.com>
2023-03-23 14:25:30 +0100

    fix(copyright): update copyright year

--- matr1x/control/__init__.py
+++ matr1x/control/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of a software collection for data aquisition (matr1x).
 # ---
-# (c) 2022 matr1x developers. All rights reserved.
+# (c) 2023 matr1x developers. All rights reserved.
 # ---
 from .controlwindow import ControlWindow  # noqa: F401
 from .util import (GuiDict, OutputRedirection, QtGracefulKiller,  # noqa: F401
