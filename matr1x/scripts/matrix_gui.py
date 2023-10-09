130006dad35eeea6f1bbe8a5c79c34a71f2ed924
Dominik Kriegner <dominik.kriegner@gmail.com>
2023-10-09 17:35:45 +0200

    fix(GUI): remove desktop file extenstion
    This suppresses a warning which is otherwise shown and seems to work for
    both PyQt5 and PyQt6 in the same way. (only the PyQt6 documention
    mentions this explicitly)

--- matr1x/scripts/matrix_gui.py
+++ matr1x/scripts/matrix_gui.py
@@ -472,7 +472,7 @@ def main():
             "The executable name 'matrix_gui' is deprecated. Use 'matrix-gui' instead.",
             FutureWarning)
     app = QApplication(sys.argv)
-    app.setDesktopFileName("matrix-gui.desktop")
+    app.setDesktopFileName("matrix-gui")
     # we need to ignore this signal here otherwise we are kicked into
     # background when matrix returns. see run_as_fg_process
     if 'SIGTTOU' in dir(signal):  # signal only on POSIX compliant systems
