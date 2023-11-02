cd466cfd6a182ed22bd2a47f4fa9529a5cedce47
pheowl <pheowl@users.noreply.github.com>
2023-11-02 11:03:55 +0100

    feat(system_picovna,-pico): added new driver for picovna running with… (#427)
    * feat(system_picovna,-pico): added new driver for picovna running with picovna5 software
    
    * pep8formatting action fixes (#428)
    
    Co-authored-by: pheowl <pheowl@users.noreply.github.com>
    
    * fix(pico.py): fix typos, wrong code and linter-induced errors
    
    * fix(pico): some more linter fixes
    
    * pep8formatting action fixes (#429)
    
    Co-authored-by: pheowl <pheowl@users.noreply.github.com>
    
    * fixes after testing
    
    * pep8formatting action fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: sysphos <sysphos>

--- matr1x/devices/__init__.py
+++ matr1x/devices/__init__.py
@@ -6,6 +6,7 @@ import logging
 
 from .boss import BOSS  # noqa: F401
 from .cryogenics import CryogenicBipolarPS, CryogenicPS  # noqa: F401
+from .danfysik import Danfysik9100  # noqa: F401
 from .keithley import (Keithley2000, Keithley2182A, Keithley2400,  # noqa: F401
                        Keithley2450, Keithley2611A, Keithley2701, Keithley6221)
 from .keysight import KeysightB2961, PNA5225b  # noqa: F401
@@ -17,6 +18,7 @@ from .oxford_mercury import (MercuryIPS, MercuryITC,  # noqa: F401
                              MercurySingleAxisIPS)
 from .pfeiffer import MPT200  # noqa: F401
 from .physikinstrumente import MercuryC663  # noqa: F401
+from .pico import PicoVNA  # noqa: F401
 from .standa import Standa8SMC1  # noqa: F401
 from .visadevice import VisaDevice  # noqa: F401
 
