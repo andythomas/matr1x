9f8ecf6de3cf69f177b4beeee20d063fe0258d31
Andy Thomas <andythomas@web.de>
2026-08-06 09:43:45 +0200

    iMac ty clean (Py3.14)

--- matr1x/__init__.py
+++ matr1x/__init__.py
@@ -110,7 +110,7 @@ __all__ = [
 if sys.version_info >= (3, 11):
     import tomllib
 else:
-    import tomli as tomllib  # ty: ignore [unresolved-import]
+    import tomli as tomllib
 
 try:
     __version__ = version("matr1x-measurements")
